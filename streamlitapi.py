68de75575ac7eaad98c3814bcd755d7ddb1b087b
Isaac Owusu Akowuah <ioakowuah@gmail.com>
2025-06-15 11:41:51 +0000

    Update streamlitapi.py

--- streamlitapi.py
+++ streamlitapi.py
@@ -1,34 +1,36 @@
 import pickle 
+import joblib
 import streamlit as st
 
 with open("RandomForestClassifier_model.pkl", "rb") as f:
     model = joblib.load(f)
-
+    
 def main():
     st.title('Client will subscribe to a term deposit')
 
     #input Variables
-    Age = st.numeric_input('age')
+    Age = st.number_input('age')
     Job = st.text_input('job')
     Marital = st.text_input('marital')
     Education = st.text_input('education')
     Default = st.text_input('default')
-    Balance = st.numeric_input('balance')
+    Balance = st.number_input('balance')
     Housing = st.text_input('housing')
     Loan = st.text_input('loan')
     Contact = st.text_input('contact')
-    Day = st.numeric_input('day')
+    Day = st.number_input('day')
     Month = st.text_input('month')
-    Duration = st.numeric_input('duration')
-    Campaign = st.numeric_input('campaign')
-    Pdays = st.numeric_input('pdays')
-    Previous = st.numeric_input('previous')
+    Duration = st.number_input('duration')
+    Campaign = st.number_input('campaign')
+    Pdays = st.number_input('pdays')
+    Previous = st.number_input('previous')
     Poutcome = st.text_input('poutcome')
 
     #prediction code
     if st.button('Predict'):
         makeprediction = model.predict([[Age,Job,Marital,Education,Default,Balance,Housing,Loan,Contact,Day,Month,Duration,Campaign,Pdays,Previous,Poutcome]])
-        output = makeprediction
+        st.write(f'Prediction: {makeprediction[0]}')
 
-    if __name__=='__main__':
-        main()
+# ⬇️ THIS PART MUST BE OUTSIDE main()
+if __name__=='__main__':
+    main()
